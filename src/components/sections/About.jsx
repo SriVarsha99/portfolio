19e2e557b1a19550171b943d8b3217d60141d50e
Srivarsha <srivarsha.gowdara@gmail.com>
2025-03-03 22:24:18 -0800

    Skills added

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,4 +1,7 @@
 export const About = () => {
+    const frontendSkills = ["React", "Vue", "Typescript", "Tailwindcss", "Svelte"];
+    const backendSkills = ["Node.js", "Python", "GarphQl"];
+
     return <section id="about" className="min-h-screen flex items-center justify-center py-20">
 
         <div className="max-w-3xl mx-auto px-4">
@@ -10,11 +13,31 @@ export const About = () => {
                 <p className="text-gray-300 mb-6">
                 The prospect of contributing to innovative applications that blend AI with interactive user interfaces is truly exciting. I am eager to bring my technical skills, creativity, and problem-solving abilities to your team and help drive VOSYN's vision forward.
                 </p>
-                <div className="grid grid-cols-1 mb:grid-cols-2 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
                     <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                         <h3 className="text-xl font-bold mb-4">Frontend</h3>
                         <div className="flex flex-wrap gap-2">
+                            {frontendSkills.map((tech,key) => (
+                                <span 
+                                key = {key}
+                                className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
 
+                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                        <h3 className="text-xl font-bold mb-4">Backend</h3>
+                        <div className="flex flex-wrap gap-2">
+                            {backendSkills.map((tech,key) => (
+                                <span 
+                                key = {key}
+                                className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                    {tech}
+                                </span>
+                            ))}
                         </div>
                     </div>
 
